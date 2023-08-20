6820a0766f38c5dd64d7af58757eaa1e9bbc471c
mogbonna <mogbonna045@gmail.com>
2023-08-20 04:37:14 -0700

    from Moses Branch

---
+++ printf_project/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/*This file is to keep the function prototypes*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
