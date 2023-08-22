5093dfe0b4310f9ac820cea9ffa7ed7690c7c0c0
Solomon <solomonchamamme@gmail.com>
2023-08-22 10:48:29 +0000

    we should use _putchar

--- m_char.c
+++ m_char.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * m_char - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int m_char(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
